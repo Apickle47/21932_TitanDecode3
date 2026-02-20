9096c5d371fc6629788f2a23446cebe47ce234bb
Apickle47 <arushduvuru@gmail.com>
2026-02-19 22:15:33 -0600

    new tele

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RedTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.config.Config;
+//import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+//import com.acmerobotics.roadrunner.Pose2d;
+
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.BottomSensor;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Gate;
+import org.firstinspires.ftc.teamcode.subsystems.Hood;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.MiddleSensor;
+import org.firstinspires.ftc.teamcode.subsystems.Mortar;
+import org.firstinspires.ftc.teamcode.subsystems.Rail;
+import org.firstinspires.ftc.teamcode.subsystems.Signal;
+import org.firstinspires.ftc.teamcode.subsystems.Tilt;
+import org.firstinspires.ftc.teamcode.subsystems.TopSensor;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.subsystems.Util;
+
+import java.util.Objects;
+
+@Configurable
+@TeleOp(name = "RedTeleOp")
+public class RedTeleOp {
+    
+}
